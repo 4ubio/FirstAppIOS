d28480b1981e4b05d49921e760660c3329272947
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-08-26 18:15:45 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Sebastián Rubio on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
